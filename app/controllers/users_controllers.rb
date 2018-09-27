8f8db8dc41b32261f700716d6cf16c9ad2276dce
81Jeremiah <jeremiahlutes@gmail.com>
2018-09-27 09:07:29 -0700

    added secure password and user controller

---
+++ app/controllers/users_controllers.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  before_action :require_login , only:[:show]
+
+def new
+  @user = User.new
+end
+
+def index
+
+end
+def show
+  @user = User.find_by(id: params[:id])
+end
+
+def create
+  @user = User.new(user_params)
+  if @user.save
+    session[:user_id] = @user.id
+    redirect_to @user
+  else
+    render :new
+  end
+
+end
