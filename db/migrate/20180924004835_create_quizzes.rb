4ce2b07bcaf6fdb013f5c1de7e4d7ee5a7678122
github username <github email address>
2018-09-29 13:50:58 -0700

    created quiz model

--- db/migrate/20180924004835_create_quizzes.rb
+++ db/migrate/20180924004835_create_quizzes.rb
@@ -1,6 +1,11 @@
 class CreateQuizzes < ActiveRecord::Migration[5.2]
   def change
     create_table :quizzes do |t|
+      t.string :name
+      t.integer :question_id
+      t.integer :answer_id
+      t.integer :quiz_score_id
+      t.integer :user_id
 
       t.timestamps
     end
