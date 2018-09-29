4ff3b8120d3ca7b638611fff6076a59966275e70
github username <github email address>
2018-09-29 13:52:50 -0700

    correct routes with to:

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :index, :show]
 
 
-  get "/signin" to: 'sessions#new'
-  post "/signin"  to:'sessions#create'
-  delete "logout"  to: 'sessions#destroy'
-  post "/rides/new" to: 'rides#new'
+  get "/signin", to: 'sessions#new'
+  post "/signin",  to:'sessions#create'
+  delete "logout",  to: 'sessions#destroy'
+  post "/rides/new", to: 'rides#new'
 
 
 end
