8ef7288498a16883230bcc881d4751f6430c931e
github username <github email address>
2018-09-29 17:24:31 -0700

    allowed nested attrbutes in order to submit quiz form

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,15 @@
 class Quiz < ApplicationRecord
 	has_many :questions
-	has_many :answers
-	
+	has_many :answers   #, through: :questions
+	accepts_nested_attributes_for :answers
+	accepts_nested_attributes_for :questions
+
+
+	# def question=(question)
+  #   self.question = Question.create(question: question)
+  # end
+	#
+	# def answer=(answer)
+	#   self.answer = Answer.create(answer: answer)
+	# end
 end
