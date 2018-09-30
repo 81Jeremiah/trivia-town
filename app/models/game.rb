29d1b181e1301ed3571cb8abda63d386dffedeab
github username <github email address>
2018-09-29 21:07:21 -0700

    fixed game logic to test answer

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,14 +1,20 @@
 class Game < ApplicationRecord
+  belongs_to :user
+  belongs_to :quiz
 
-  def play_game
-     user.answer_array.each do |answer|
-       if answer == quiz_anwer
-         user_game_score + 1
-       end
-       user.game_score.save
-     end
+  def self.play_game(game)
+    if game.guess == game.quiz.answer
+      "congratulaions"
+    end
   end
-
+  # def play_game
+  #    user.answer_array.each do |answer|
+  #      if answer == quiz_anwer
+  #        user_game_score + 1
+  #      end
+  #      user.game_score.save
+  #    end
+  # end
 
 end
   #user.quiz.question.answer = user.quiz.question.guess
