53a2342367c34590d3c4a6f36ccdaa4d1585344b
github username <github email address>
2018-09-29 21:09:36 -0700

    added only games route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :index, :show]
   resources :quizzes
+  #resources :games
 
   get "/signin", to: 'sessions#new'
   post "/signin",  to:'sessions#create'
   delete "logout",  to: 'sessions#destroy'
-  post "/rides/new", to: 'rides#new'
+  post "/games/new" => 'games#new'
 
 
 end
