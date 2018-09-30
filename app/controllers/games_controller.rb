8cf19c7cdcf0ed3cd0d2aad29af3fa842e1a0a91
github username <github email address>
2018-09-29 21:06:34 -0700

    created abilty to play game and verify answer

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,15 @@
 class GamesController < ApplicationController
 
     def new
-      game = Game.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
+      @game = Game.new
+      @quiz = Quiz.find_by(id: params[:quiz_id])
+      @game.user_id = current_user.id
+      @game.quiz_id = @quiz.id
+      @game.guess = params[:guess]
+      @game.save
+      message = Game.play_game(@game)
+      #game = Game.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
+      redirect_to quiz_path(@quiz, message: message)
 
     end
 
