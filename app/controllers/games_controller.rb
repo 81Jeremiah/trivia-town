3328246cade746f2c3f30a26ae01036e2c6c0d20
github username <github email address>
2018-10-01 06:39:53 -0700

    cleaned up controller to update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,10 +3,12 @@ class GamesController < ApplicationController
     def new
       @game = Game.new
       @quiz = Quiz.find_by(id: params[:quiz_id])
-      @game.user_id = current_user.id
-      @game.quiz_id = @quiz.id
-      @game.guess = params[:guess]
-      @game.save
+      #binding.pry
+      @game.update(user_id:current_user.id, quiz_id: @quiz.id, guess: params[:guess])
+      # @game.user_id = current_user.id
+      # @game.quiz_id = @quiz.id
+      # @game.guess = params[:guess]
+      # @game.save
       message = Game.play_game(@game)
       #game = Game.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
       redirect_to quiz_path(@quiz, message: message)
