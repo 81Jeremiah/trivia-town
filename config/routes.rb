d432a81a6c1b7ae6aa22f694e625a7cc4adc778e
github username <github email address>
2018-10-02 21:34:56 -0700

    fixed route for all game

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :index]
   resources :quizzes
-  #resources :games
+  resources :games
 
   get "/signin", to: 'sessions#new'
   post "/signin",  to:'sessions#create'
   delete "logout",  to: 'sessions#destroy'
   post "/games/new" => 'games#new'
-  
+
   resources :users, only: [:show] do
     # nested resource for quizes
     resources :quizzes, only: [:show, :index]
