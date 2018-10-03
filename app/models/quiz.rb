98e485fa95482e2dddcda298bbe9b979a155a77a
github username <github email address>
2018-10-02 21:33:23 -0700

    have quizz alloq question and answer model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,10 +1,11 @@
 class Quiz < ApplicationRecord
-	has_many :questions
-	has_many :answers   #, through: :questions
-	has_many :games
-	accepts_nested_attributes_for :answers
-	accepts_nested_attributes_for :questions
-
+	# has_many :questions
+	# has_many :answers   #, through: :questions
+	# has_many :games
+	# accepts_nested_attributes_for :answers
+	# accepts_nested_attributes_for :questions
+  has_many :question_and_answers
+	accepts_nested_attributes_for :question_and_answers
 
 	# def question=(question)
   #   self.question = Question.create(question: question)
