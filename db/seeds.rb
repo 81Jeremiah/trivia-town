70d7f89ef7dd0a341ff255404407453ca53c74d2
github username <github email address>
2018-10-06 17:53:16 -0700

    added seeds to see categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create([{ name: 'Sports' }, { name: 'Movies' }, { name: 'History' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
