2d1959f6917099271ba1dc66dcd71bebee0f5da1
github username <github email address>
2018-10-06 17:52:26 -0700

    added omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,17 @@ Rails.application.routes.draw do
   resources :quizzes, only: [:show] do
   	resources :games, only: [:new]
   end
-  get "/signin", to: 'sessions#new'
-  post "/signin",  to:'sessions#create'
+  get "/login", to: 'sessions#new'
+  post "/login",  to:'sessions#create'
   delete "logout",  to: 'sessions#destroy'
   #post "/games/new" => 'games#new'
+  get '/auth/facebook/callback' => 'sessions#create'
 
   resources :users, only: [:show] do
     # nested resource for quizes
     resources :quizzes, only: [:show, :index]
   end
+
+  root 'quizzes#index'
 
 end
