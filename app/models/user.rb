3430f7b3253f89ee7a69ee79bb559568dd275805
github username <github email address>
2018-10-06 17:49:03 -0700

    corrected validations for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
 	has_many :games
 	has_many :quiz_scores
 
-	validates :name, presence: true
-    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
-    validates :email, uniqueness: true
+	validates :username, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+  validates :email, uniqueness: true
+
+	
 end
