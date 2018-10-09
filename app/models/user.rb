6782592414d9b951240457adfbc16b11c2d57c05
github username <github email address>
2018-10-08 22:07:38 -0700

    user can have many comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
 	has_secure_password
 	has_many :games
 	has_many :quiz_scores
-
+  has_many :comments
+	has_many :quizzes, through: :comments
 	validates :username, presence: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
   validates :email, uniqueness: true
 
-	
+
 end
