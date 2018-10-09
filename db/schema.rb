ab578f1a69559d73f5ab02d46be3674631e1e120
github username <github email address>
2018-10-08 22:09:35 -0700

    added comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_07_003750) do
+ActiveRecord::Schema.define(version: 2018_10_09_032010) do
 
   create_table "answers", force: :cascade do |t|
     t.text "answer"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2018_10_07_003750) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "quiz_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quiz_id"], name: "index_comments_on_quiz_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "games", force: :cascade do |t|
