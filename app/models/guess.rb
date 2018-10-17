c0a507185b62ab5121b512fdc851be0edd6a3ca5
github username <github email address>
2018-10-17 08:25:02 -0700

    submitted answers and guesses are only downcase in database

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,3 +1,9 @@
 class Guess < ApplicationRecord
   belongs_to :game
+  before_save :downcase_fields
+
+
+  def downcase_fields
+      self.guess.downcase!
+   end
 end
--- app/models/question_and_answer.rb
+++ app/models/question_and_answer.rb
@@ -1,7 +1,13 @@
 class QuestionAndAnswer < ApplicationRecord
   belongs_to :quiz
+
   validates :question, presence: true
   validates :answer, presence: true
+  before_save :downcase_fields
 
+
+  def downcase_fields
+      self.answer.downcase!
+   end
 
  end
