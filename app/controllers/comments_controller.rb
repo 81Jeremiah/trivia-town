54d3f528164ca40782996b92675eec2fa10665d5
github username <github email address>
2018-10-17 21:34:42 -0700

    require login to create comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,15 @@
 class CommentsController < ApplicationController
+  before_action :require_login
 
-def create
-  if params[:quiz_id]
-   @quiz = Quiz.find(params[:quiz_id])
-   @comment = @quiz.comments.build(comment_params)
-   @comment.user = current_user
-   @comment.save
-   redirect_to @quiz
- end
-end
+  def create
+    if params[:quiz_id]
+     @quiz = Quiz.find(params[:quiz_id])
+     @comment = @quiz.comments.build(comment_params)
+     @comment.user = current_user
+     @comment.save
+     redirect_to @quiz
+   end
+  end
 
 private
   def comment_params
