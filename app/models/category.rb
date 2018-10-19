9740450ac63c2d831ad5c91bf71482c3d9e83c2d
github username <github email address>
2018-10-18 20:36:07 -0700

    added some css classes to improve overall look

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,11 @@
 class Category < ApplicationRecord
   has_many :quiz_categories
   has_many :quizzes, through: :quiz_categories
+  before_save :downcase_fields
+
+  def downcase_fields
+      self.name.downcase!
+   end
 
   def to_param
     name
