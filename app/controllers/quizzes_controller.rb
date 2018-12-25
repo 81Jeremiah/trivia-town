79800f70bc8f222f68af0f875cb5618af0f13054
github username <github email address>
2018-12-24 20:03:37 -0500

    convert to Json in quiz controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -24,12 +24,13 @@ class QuizzesController < ApplicationController
   def create
   	@quiz = Quiz.new(quiz_params)
     @quiz.user_id = current_user.id
-     if
-       @quiz.save
-       redirect_to quiz_path(@quiz)
-     else
-       render :new
-     end
+    render json: @quiz, status: 201
+     # if
+     #   @quiz.save
+     #   redirect_to quiz_path(@quiz)
+     # else
+     #   render :new
+     # end
    end
 
   def edit
