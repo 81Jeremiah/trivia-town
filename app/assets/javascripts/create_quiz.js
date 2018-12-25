b166861438f92ac5eda2a6e7a5aa383e3005b307
github username <github email address>
2018-12-24 21:08:59 -0500

    corrected search to get correct text in jquery

--- app/assets/javascripts/create_quiz.js
+++ app/assets/javascripts/create_quiz.js
@@ -13,17 +13,18 @@ function attachListeners(){
     posting.done(function(data) {
         var quiz = data;
          $("#quizName").text(quiz["name"]);
-         $("#question1").text(quiz[question_and_answers_attributes][0][question]);
-         $("#answer1").text(quiz[question_and_answers_attributes][0][answer]);
-         $("#question2").text(quiz[question_and_answers_attributes][1][question]);
-         $("#answer2").text(quiz[question_and_answers_attributes][1][answer]);
-         $("#question3").text(quiz[question_and_answers_attributes][2][question]);
-         $("#answer3").text(quiz[question_and_answers_attributes][2][answer]);
-         $("#question4").text(quiz[question_and_answers_attributes][3][question]);
-         $("#answer4").text(quiz[question_and_answers_attributes][3][answer]);
-         $("#question5").text(quiz[question_and_answers_attributes][4][question]);
-         $("#answer5").text(quiz[question_and_answers_attributes][4][answer]);
+         $("#question1").text(quiz.question_and_answers[0].question);
+         $("#answer1").text(quiz.question_and_answers[0].answer);
+         $("#question2").text(quiz.question_and_answers[1].question);
+         $("#answer2").text(quiz.question_and_answers[1].answer);
+         $("#question3").text(quiz.question_and_answers[2].question);
+         $("#answer3").text(quiz.question_and_answers[2].answer);
+         $("#question4").text(quiz.question_and_answers[3].question);
+         $("#answer4").text(quiz.question_and_answers[3].answer);
+         $("#question5").text(quiz.question_and_answers[4].question);
+         $("#answer5").text(quiz.question_and_answers[4].answer);
+         debugger
     })
-    debugger
+
   })
 }
