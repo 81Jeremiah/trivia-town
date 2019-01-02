838c0569be70d5528b97f93a63dcdb1e4e41ff43
github username <github email address>
2019-01-01 20:20:07 -0800

    ids added to rendor json form

--- app/assets/javascripts/create_quiz.js
+++ app/assets/javascripts/create_quiz.js
@@ -23,8 +23,32 @@ function attachListeners(){
          $("#answer4").text(quiz.question_and_answers[3].answer);
          $("#question5").text(quiz.question_and_answers[4].question);
          $("#answer5").text(quiz.question_and_answers[4].answer);
-         debugger
+
     })
 
   })
+  $('#category').change(() =>{
+    let cat = $('#category').val();
+    debugger
+    $.get("/quizzes/categories" + cat, function(data, status) {
+      console.log(data)
+      console.log(status)
+      // Replace text of body-id div
+      // $("#body-" + id).html(data["description"]);
+    });
+
+  })
+
+  $('#please-work').click((e) =>{
+    $.get("/quizzes.json", function(data){
+      console.log(data)
+    })
+    e.preventDefault();
+  })
+
 }
+
+// def show
+//   category = Category.find_by_name(params[:category])
+//   @quizzes = Quiz.by_category(category.id)
+// end
