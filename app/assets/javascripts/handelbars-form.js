0c00bf23a7392bbab992d30969fec55249ac7b9e
github username <github email address>
2019-01-08 08:34:11 -0800

    temporary handlebar form placeholder
    will refactor to build on new page and using each

--- app/assets/javascripts/handelbars-form.js
+++ app/assets/javascripts/handelbars-form.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  // let source = $("#quiz-form-template")[0].innerHTML
-  // let template = Handlebars.compile(source)
-  // let context = {name: "name"}
-  // let html = template(context)
-  var source   = document.getElementById("entry-template").innerHTML;
-  var template = Handlebars.compile(source);
-  debugger
-  var context = {title: "My New Post", body: "This is my first post!"};
-  var html    = template(context);
+  let source = document.getElementById("quiz-form-template").innerHTML
+  let template = Handlebars.compile(source)
+  let context = {name: "name", question1: "question1", answer1: "answer1",question2: "question2", answer2: "answer2", question3: "question3", answer3: "answer3", question4: "question4", answer4: "answer4", question5: "question5", answer5: "answer5"}
+  let html = template(context)
+  // var source   = document.getElementById("entry-template").innerHTML;
+  // var template = Handlebars.compile(source);
+  // debugger
+  // var context = {title: "My New Post", body: "This is my first post!"};
+  // var html    = template(context);
   $('.page-body').append(html)
 })
