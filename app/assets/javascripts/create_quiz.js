d3ee445f674f7852c7b724db0867d1dfb07818a4
github username <github email address>
2019-01-13 19:52:19 -0800

    moved new ceated quiz to class and updated how it renders to stay DRY

--- app/assets/javascripts/create_quiz.js
+++ app/assets/javascripts/create_quiz.js
@@ -6,36 +6,42 @@ function attachListeners(){
   $( document ).delegate('form[id=new_quiz]', "submit", function(e){
 
   // $('form[id=new_quiz]').submit((e) => {
-     alert('is this working?')
     e.preventDefault();
 
     var values = $(this).serialize();
-    debugger
+
     var posting = $.post('/quizzes', values);
-    debugger
+
 
     posting.done(function(data) {
       $('#quiz-form').empty()
-
-        var quiz = data;
-         $("#quizName").text(quiz["name"]);
-         $("#question1").text(quiz.question_and_answers[0].question);
-         $("#answer1").text(quiz.question_and_answers[0].answer);
-         $("#question2").text(quiz.question_and_answers[1].question);
-         $("#answer2").text(quiz.question_and_answers[1].answer);
-         $("#question3").text(quiz.question_and_answers[2].question);
-         $("#answer3").text(quiz.question_and_answers[2].answer);
-         $("#question4").text(quiz.question_and_answers[3].question);
-         $("#answer4").text(quiz.question_and_answers[3].answer);
-         $("#question5").text(quiz.question_and_answers[4].question);
-         $("#answer5").text(quiz.question_and_answers[4].answer);
+        let quiz = new Quiz(data)
+        quiz.createHtmlForQuizName()
+        quiz.createHtmlForCategories()
+        quiz.createHtmlForQuestionsAndAnswers()
+        //  debugger
+        // var quiz = data;
+        //  $("#quizName").text(quiz["name"]);
+        //  $("#question1").text(quiz.question_and_answers[0].question);
+        //  $("#answer1").text(quiz.question_and_answers[0].answer);
+        //  $("#question2").text(quiz.question_and_answers[1].question);
+        //  $("#answer2").text(quiz.question_and_answers[1].answer);
+        //  $("#question3").text(quiz.question_and_answers[2].question);
+        //  $("#answer3").text(quiz.question_and_answers[2].answer);
+        //  $("#question4").text(quiz.question_and_answers[3].question);
+        //  $("#answer4").text(quiz.question_and_answers[3].answer);
+        //  $("#question5").text(quiz.question_and_answers[4].question);
+        //  $("#answer5").text(quiz.question_and_answers[4].answer);
 
     })
   })
 
   $('#category').change(() =>{
     alert("I changed this")
-    $('.page-body').empty()
+    $('.list-quizzes').empty()
+    $('.list-quizzes').append('<tbody><tr><td>Quiz Name</td><td>Created By</td><td>The Top Score</td></tr></tbody>')
+
+
     let cat = $('#category').val();
     // debugger
     $.get("/quizzes/categories/" + cat, function(data) {
@@ -44,7 +50,7 @@ function attachListeners(){
      // let name = data["0"].name
      data.forEach((e)=> {
        quiz = new Quiz(e)
-     $('.page-body').append('<p>' + quiz.createLi() + '</p>');
+     $('.list-quizzes tr:last').after(quiz.createTr());
    });
 
     });
@@ -60,17 +66,23 @@ function attachListeners(){
 
   $('.all-quizzes').click((e) =>{
     alert('was this clicked?')
-    $('.page-body').empty()
+    $('.welcome-page').hide()
+    $('#quiz-name').text('Quiz Name')
+    $('#created-by').text('Created By')
+    $('#top-score').text('The Top Score')
 
-    $.get("/quizzes", function(data){
-      $('.page-body').html(data)
-      // data.forEach((e)=> {
+    $.get("/quizzes.json", function(quizzes){
+      quizzes.forEach((e)=> {
+        quiz = new Quiz(e)
+
+      $('.list-quizzes tr:last').after(quiz.createTr())
+
       // $('.page-body').append(`<p><a class="list-quizzes" id="quiz-${e.id}" href='/quizzes/${e.id}'>${e.name}</a> <br> ${e.games.length} </p>`);
       // })
     })
-    e.preventDefault();
   })
-
+  e.preventDefault();
+})
   // $("#quiz-1").on('click', function(e){
   $( document ).delegate('.indv-quiz', "mouseover", function(e){
   // $('.indv-quiz').on("mouseover", function(e) {
@@ -124,18 +136,31 @@ function attachListeners(){
     $.get('/users/games_users_played', function(games){
       $('#games-played').html(games)
     })
-      $('#games-played').toggle();
+    $('#load-games').hide();
     e.preventDefault()
   })
 
+   $('#hide-games').click((e) =>{
+     $('#games-played').hide();
+
+     e.preventDefault()
+   })
+
   $('#load-quizzes-created').click((e) => {
-    $.get('users/quizzes_user_created', function(createdQuizzes){
+    alert('whatz up')
+    $.get('/users/quizzes_user_created', function(createdQuizzes){
       $('#quizzes-created').html(createdQuizzes)
 
     })
-    $('#quizzes-created').toggle();
+    $('#load-quizzes-created').hide();
     e.preventDefault()
   })
+
+  $('#hide-quizzes').click((e) => {
+    $('#quizzes-created').hide();
+    e.preventDefault()
+  })
+
 }
 
 function loadQuizForm() {
@@ -157,14 +182,39 @@ class Quiz {
     this.userId = quizJson.user_id
     this.scores = quizJson.games.map((game) => game.score)
     this.highScore = this.scores.length === 0 ? 0 :  Math.max.apply(null,this.scores)
+    this.questionAndAnwers = quizJson.question_and_answers
+    this.categories = quizJson.categories.map((category) => category.name)
     // this.highScore = 0 || Math.max.apply(null,this.scores)
 
    }
-
+   createTr() {
+     return `<tr> <td> <a href="quizzes/${this.id}">${this.name} </a></td> <td> <a href="users/${this.userId}"> ${this.userName} </td> <td> ${this.highScore} </td> </tr>`
+   }
    createLi() {
      return `Quiz Name: <a href="quizzes/${this.id}">${this.name} </a> Created By: <a href="users/${this.userId}"> ${this.userName} </a> High Score: ${this.highScore}`
    }
-}
+
+   createHtmlForQuizName(){
+     return  $("#quizName").text(`Quiz Name: ${this.name}`);
+   }
+   createHtmlForCategories(){
+     let listEach = () => {
+       this.categories.forEach((category) => {
+         category + '&nbsp'
+       })
+     }
+     return $('#categories').text(`Categories: ${listEach()}`)
+     }
+
+   createHtmlForQuestionsAndAnswers(){
+     let i = 0
+     for (i = 0; i < this.questionAndAnwers.length; i++) {
+       return $(`#question${i+1}`).text(this.questionAndAnwers[i][0]);
+              $(`#answer${i+1}`).text(this.questionAndAnwers[i][1])
+     }
+   }
+ }
+
 
 // def show
 //   category = Category.find_by_name(params[:category])
