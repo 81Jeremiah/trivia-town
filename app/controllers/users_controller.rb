e2b09359822a12e7eb79f276e627f0be7e6ff1ba
github username <github email address>
2019-01-27 12:02:18 -0800

    fixed route so render layout false works properly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,37 @@
 class UsersController < ApplicationController
   before_action :require_login , only:[:show]
 
-def new
-  @user = User.new
-end
-
-def index
-
-end
-
-def show
-  @user = User.find_by(id: params[:id])
-end
-
-def create
-  @user = User.new(user_params)
-  if @user.save
-    session[:user_id] = @user.id
-    redirect_to @user
-  else
-    render :new
+  def new
+    @user = User.new
   end
 
-end
+  def index
 
-def games_users_played
-  render 'users/games_users_played', layout: false
-end
+  end
 
-def games_user_created
-  render 'users/quizzes_user_created', layout: false
-end
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      render :new
+    end
+
+  end
+
+  def quizzes_user_created
+    render '/users/quizzes_user_created', layout: false
+  end
+
+  def games_users_played
+    render '/users/games_users_played', layout: false
+  end
+
 
 private
   def user_params
