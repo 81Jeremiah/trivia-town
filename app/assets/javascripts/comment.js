d34073e2a11d0d9605df763cdc10f1e057c497e8
github username <github email address>
2019-02-03 13:36:25 -0800

    added class contructor for comment

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,23 +1,34 @@
-function init() {
-  //put any page initialization/handlebars initialization here
-  initForm()
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  init()
+//add comment form to page
+$( document ).ready(() => {
+  $("#add-comment").click((e) => {
+    $.get(e.target.href,(data) =>{
+      $(".new-comment-form").html(data)
+    })
+    e.preventDefault()
+  })
+//submit comment and render back in Json
+$( document ).delegate('form[id=new_comment]', "submit", function(e){
+    e.preventDefault();
+    let values = $(this).serialize();
+    let href = this.action
+    let posting = $.post(href, values);
+    posting.done(function(data) {
+      let comment = new Comment(data)
+      let newComment = comment.createComment()
+      $('.comment-list').append(newComment)
+      $('#comment_content').val(" ")
+    });
+  })
 })
+//build class and use protype to build html for comment
+class Comment{
+  constructor(commentJson) {
+    this.content = commentJson.content
+    this.username = commentJson.user.username
+    this.userId = commentJson.userId
 
-
-function initForm() {
-  let formTemplate = document.getElementById("comment-form").innerHTML
-  let template = Handlebars.compile(formTemplate)
-
-  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createComment()'})
-}
-
-  // $(document.body).on("submit", "#submit-comment", function() {alert("hey");});
-
-function createComment() {
-  let recipe = $("#new-comment").val()
-
-}
+   }
+   createComment() {
+     return `<p> <a href="users/${this.userId}">${this.username}</a> said: ${this.content} </p>`
+   }
+ }
