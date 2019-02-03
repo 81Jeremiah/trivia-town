03b58ac5524156f72b7ee53346afab3ac45a2592
github username <github email address>
2019-02-03 09:01:33 -0800

    cleaned up quiz serializer for only what is needed

--- app/serializers/quiz_serializer.rb
+++ app/serializers/quiz_serializer.rb
@@ -1,9 +1,8 @@
 class QuizSerializer < ActiveModel::Serializer
-  attributes :id, :name, :quiz_score_id, :user_id
+  attributes :id, :name, :user_id
   belongs_to :user
   has_many :games, serializer: QuizGameSerializer
   has_many :question_and_answers
   # has_many :quiz_categories
   has_many :categories, through: :quiz_categories
-  has_many :users, through: :games
 end
