057e38e5bd24bad13b6800c5fd66adcc3e02d7a7
github username <github email address>
2019-02-03 15:28:46 -0800

    erased unused attributed from serializer

--- app/serializers/question_and_answer_serializer.rb
+++ app/serializers/question_and_answer_serializer.rb
@@ -1,6 +1,5 @@
 class QuestionAndAnswerSerializer < ActiveModel::Serializer
   attributes :question,:answer, :id, :quiz_id
-  # belongs_to :quiz, serializer: QuizQuestionAndAnswerSerializer
 
 
 end
