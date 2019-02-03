21c96a85d671d72526506eacfe4d2ab528d42849
github username <github email address>
2019-02-03 15:05:25 -0800

    load all quizzes in all search bar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,19 @@
 class CategoriesController < ApplicationController
   before_action :require_login
-
+#render all categories if all selected
+  def index
+    @quizzes = Quiz.all
+    render json: @quizzes, status: 200
+  end
 #show page by category chosen
    def show
      category = Category.find_by_name(params[:id])
-
      @quizzes = Quiz.by_category(category.name)
      render json: @quizzes, status: 200
     end
+
+  def searhBar
+    render '/categories/category_search_bar', layout: false
+  end
+
   end
